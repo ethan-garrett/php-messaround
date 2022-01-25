f02f11fcbba4a0a040043a6e5847ed4f04dbe1ff
Ethan Garrett <garrettethan63@gmail.com>
2022-01-25 11:26:36 +0000

    Added array.php file

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+$arrays = [1,2,3,4,5,6,7,8];
+
+$arrays[] = 9;
+array_push($arrays, 10);
+echo '<pre>';
+var_dump($arrays);
+echo '</pre>';
+
+shuffle($arrays);
+echo '<pre>';
+var_dump($arrays);
+echo '</pre>';
+
+foreach ($arrays as $array) {
+    if ($array % 2 !== 0 ){
+        echo $array;
+    }
+}
