464b157919bf74cde5d274680329807425576d1d
Ethan Garrett <garrettethan63@gmail.com>
2022-01-26 10:28:34 +0000

    modified function

--- function.php
+++ function.php
@@ -5,12 +5,14 @@ $num2 = 8;
 
 
 /**
+ * Takes an integer, squares it , then adds a second integer
+ *
  * @param $num1 int the number being squared
  * @param $num2 int the number added to num1 squared
  * @return int result of num1 squared added to num2
  */
 
-function squareAdd (int $num1 , int $num2 = 0) {
+function squareAdd (int $num1 , int $num2 = 0): int {
     $squared = $num1 * $num1;
     $squared += $num2;
     return $squared;
