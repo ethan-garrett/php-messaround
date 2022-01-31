54398251591b06d40c5831bc3b7b6531b0c4f51e
Ethan Garrett <garrettethan63@gmail.com>
2022-01-31 16:48:07 +0000

    Creted unit testing for login button

--- unitTestingEx/tests/loginButtonTest.php
+++ unitTestingEx/tests/loginButtonTest.php
@@ -5,5 +5,16 @@ require '../loginButton.php';
 use PHPUnit\Framework\TestCase;
 
 class loginButtonTest extends TestCase {
-
+    public function testSuccessLoginTrue() {
+        $expected = '<a href="#">Logout</a>';
+        $login = true;
+        $case = loginButton($login);
+        $this->assertEquals($expected, $case);
+    }
+    public function testSuccessLoginFalse() {
+        $expected = '<a href="#">Login</a>';
+        $login = false;
+        $case = loginButton($login);
+        $this->assertEquals($expected, $case);
+    }
 }
